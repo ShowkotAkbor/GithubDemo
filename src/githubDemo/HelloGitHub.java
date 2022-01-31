9e998d900638c6e99d66c2c838b7af1da6d4b429
showkot akbor <showkotakbor@showkots-MBP.fios-router.home>
2022-01-31 10:38:23 -0500

    01312022 10.38am

---
+++ src/githubDemo/HelloGitHub.java
@@ -0,0 +1,6 @@
+package githubDemo;
+public class HelloGitHub {
+	public static void main(String[] arg) {
+		System.out.println("I know how to create a seperate remote repository in Github and can push my project GithubDemo,without help");
+}
+}
